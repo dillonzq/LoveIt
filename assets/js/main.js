d85784c9c95cc7a155729aab5ae6eb47c59148e0
Giuseppe Pignataro <rogepix@gmail.com>
2019-03-11 15:05:27 +0100

    Removed emoji in the tab
    Removed the emoji when the tab is on blur and now show the current title

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ jQuery(function($) {
     _Blog.changeTitle = function() {
         var currentTitle = document.title;
         window.onblur = function() {
-            document.title = '_(:3 」∠ )_';
+            document.title = currentTitle;
         }
         window.onfocus = function() {
             document.title = currentTitle;
