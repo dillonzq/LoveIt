45c9281131665719b9246b27056f5563c0489a76
Dillon <dillonzq@outlook.com>
2019-08-26 15:17:21 +0800

    fix: fix icon css bug

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -115,7 +115,7 @@ jQuery(function($) {
             const headers = document.querySelectorAll('.post-content>h' + num);
             for (let i = 0; i < headers.length; i++) {
                 const header = headers[i];
-                header.innerHTML = `<a href="#${header.id}" class="headerlink anchor"><i class="iconfont icon-link"></i></a>${header.innerHTML}`;
+                header.innerHTML = `<a href="#${header.id}" class="headerlink"></a>${header.innerHTML}`;
             }
         }
     };
