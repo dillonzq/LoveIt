ea81b92c46733735f56df65802f5063a0c0c6fdd
dreamsafari <mac.liang@live.com>
2020-03-29 21:04:14 +0800

    deleted:    layouts/page/search.html 	new file:   layouts/partials/_default/search.html 	modified:   static/js/search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,111 +1,96 @@
+var search = function () {
 
-summaryInclude=60;
-var fuseOptions = {
-  shouldSort: true,
-  includeMatches: true,
-  threshold: 0.0,
-  tokenize:true,
-  location: 0,
-  distance: 100,
-  maxPatternLength: 32,
-  minMatchCharLength: 1,
-  keys: [
-    {name:"title",weight:0.8},
-    {name:"contents",weight:0.5},
-    {name:"tags",weight:0.3},
-    {name:"categories",weight:0.3}
-  ]
-};
+    var searchOptions = {
+        shouldSort: true,
+        tokenize: true,
+        threshold: 0.1,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "title",
+            "content",
+            "tag"
+        ]
+    };
 
+    var searchResult = getElement("search-result", 'id'),
+        searchInput = getElement("search-input", 'id'),
+        searchInfo = getElement('search-info', 'id');
 
-var searchQuery = param("s");
-if(searchQuery){
-  $("#search-query").val(searchQuery);
-  executeSearch(searchQuery);
-}else {
-  $('#search-results').append("<p>Please enter a word or phrase above</p>");
-}
+    var posts = [],
+        fuse;
 
-
-
-function executeSearch(searchQuery){
-  $.getJSON( "/index.json", function( data ) {
-    var pages = data;
-    var fuse = new Fuse(pages, fuseOptions);
-    var result = fuse.search(searchQuery);
-    console.log({"matches":result});
-    if(result.length > 0){
-      populateResults(result);
-    }else{
-      $('#search-results').append("<p>No matches found</p>");
+    if (!posts.length) {
+        fetch('/index.json').then((res) => res.json()).then(response => { //获取内容
+            posts = response;
+            bindSearch();
+        })
+    } else {
+        bindSearch();
     }
-  });
-}
 
-function populateResults(result){
-  $.each(result,function(key,value){
-    var contents= value.item.contents;
-    var snippet = "";
-    var snippetHighlights=[];
-    var tags =[];
-    if( fuseOptions.tokenize ){
-      snippetHighlights.push(searchQuery);
-    }else{
-      $.each(value.matches,function(matchKey,mvalue){
-        if(mvalue.key == "tags" || mvalue.key == "categories" ){
-          snippetHighlights.push(mvalue.value);
-        }else if(mvalue.key == "contents"){
-          start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
-          end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
-          snippet += contents.substring(start,end);
-          snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
+    function bindSearch() {
+        fuse = new Fuse(posts, searchOptions); //初始化Fuse
+
+        if (searchInput.value !== '') { // 以防用户在数据加载之前已经输入
+            doSearch(searchInput.value);
+        };
+
+        searchInput.addEventListener('change', function (e) {
+            var value = e.target.value;
+
+            if (value == '') {
+                return;
+            };
+
+            doSearch(value);
+        });
+    };
+
+    function doSearch(keyword) {
+        searchInfo.innerHTML = ''; //清空内容
+        searchResult.innerHTML = '';
+
+        var results = fuse.search(keyword);
+
+        if (!results.length) {
+            searchInfo.innerHTML = `Have not found any post related to keyword <strong>${keyword}</strong>`;
+            return;
+        } else {
+            searchInfo.innerHTML = `Found ${results.length} posts related to keyword <strong>${keyword}</strong>`;
         }
-      });
+
+        results.forEach(function (result, i) {
+            var item = document.createElement('div');
+            var html = `<article>
+                        <header class="post-header">
+                            <h2 class='post-title'>
+                                <a href='${ result.url }'>
+                                    ${ result.title }
+                                </a>
+                            </h2>
+                        </header>
+                        <section class="post-excerpt">
+                            <p>${ result.content }</p>
+                        </section>
+                        <section class="post-meta">`;
+
+            if (result.tag.length) {
+                html += `<a href='/tag/${ encodeURIComponent(result.tag[0].toLocaleLowerCase()) }'>${result.tag[0]}</a>`;
+            };
+
+            html += `
+                        <time class="post-date">
+                                ${ result.date }
+                            </time>
+                        </section>
+                    </article>
+                `;
+            item.innerHTML = html;
+
+            searchResult.appendChild(item);
+        });
     }
-
-    if(snippet.length<1){
-      snippet += contents.substring(0,summaryInclude*2);
-    }
-    //pull template from hugo templarte definition
-    var templateDefinition = $('#search-result-template').html();
-    //replace values
-    var output = render(templateDefinition,{key:key,title:value.item.title,link:value.item.permalink,tags:value.item.tags,categories:value.item.categories,snippet:snippet});
-    $('#search-results').append(output);
-
-    $.each(snippetHighlights,function(snipkey,snipvalue){
-      $("#summary-"+key).mark(snipvalue);
-    });
-
-  });
 }
-
-function param(name) {
-    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
-}
-
-function render(templateString, data) {
-  var conditionalMatches,conditionalPattern,copy;
-  conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-  //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-  copy = templateString;
-  while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-    if(data[conditionalMatches[1]]){
-      //valid key, remove conditionals, leave contents.
-      copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
-    }else{
-      //not valid, remove entire section
-      copy = copy.replace(conditionalMatches[0],'');
-    }
-  }
-  templateString = copy;
-  //now any conditionals removed we can do simple substitution
-  var key, find, re;
-  for (key in data) {
-    find = '\\$\\{\\s*' + key + '\\s*\\}';
-    re = new RegExp(find, 'g');
-    templateString = templateString.replace(re, data[key]);
-  }
-  return templateString;
-}
-
-
