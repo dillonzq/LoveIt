39153995f2dec7b73db85fdc7e08b43012c4805c
dreamsafari <mac.liang@live.com>
2020-03-30 13:44:37 +0800

    Revert "	new file:   layouts/_default/search.html"
    This reverts commit 748a23e5ff23292aa9a61491c5f75efb6fba00e7.
    
    	deleted:    layouts/_default/search.html
    	new file:   layouts/partials/_default/search.html
    	deleted:    static/css/main.css
    	modified:   static/js/search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,127 +1,96 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  const searchButton = document.querySelector('#searchBoxButton')
-  const searchInput = document.querySelector('#searchBoxInput')
-  const searchResult = document.querySelector('#searchResult')
-  const searchCount = document.querySelector('#searchCount')
+var search = function () {
 
-  function parseLocationSearch() {
-    let locationSearch = location.search
-    let result = {}
-    if (locationSearch == '') {
-      return result
+    var searchOptions = {
+        shouldSort: true,
+        tokenize: true,
+        threshold: 0.1,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "title",
+            "content",
+            "tag"
+        ]
+    };
+
+    var searchResult = getElement("search-result", 'id'),
+        searchInput = getElement("search-input", 'id'),
+        searchInfo = getElement('search-info', 'id');
+
+    var posts = [],
+        fuse;
+
+    if (!posts.length) {
+        fetch('/index.json').then((res) => res.json()).then(response => { //获取内容
+            posts = response;
+            bindSearch();
+        })
+    } else {
+        bindSearch();
     }
-    locationSearch = locationSearch.substr(1)
-    for (let kv of locationSearch.split('&')) {
-      if (kv == '') {
-        continue
-      }
-      const [k, v] = kv.split('=')
-      result[k] = decodeURI(v)
+
+    function bindSearch() {
+        fuse = new Fuse(posts, searchOptions); //初始化Fuse
+
+        if (searchInput.value !== '') { // 以防用户在数据加载之前已经输入
+            doSearch(searchInput.value);
+        };
+
+        searchInput.addEventListener('change', function (e) {
+            var value = e.target.value;
+
+            if (value == '') {
+                return;
+            };
+
+            doSearch(value);
+        });
+    };
+
+    function doSearch(keyword) {
+        searchInfo.innerHTML = ''; //清空内容
+        searchResult.innerHTML = '';
+
+        var results = fuse.search(keyword);
+
+        if (!results.length) {
+            searchInfo.innerHTML = `Have not found any post related to keyword <strong>${keyword}</strong>`;
+            return;
+        } else {
+            searchInfo.innerHTML = `Found ${results.length} posts related to keyword <strong>${keyword}</strong>`;
+        }
+
+        results.forEach(function (result, i) {
+            var item = document.createElement('div');
+            var html = `<article>
+                        <header class="post-header">
+                            <h2 class='post-title'>
+                                <a href='${ result.url }'>
+                                    ${ result.title }
+                                </a>
+                            </h2>
+                        </header>
+                        <section class="post-excerpt">
+                            <p>${ result.content }</p>
+                        </section>
+                        <section class="post-meta">`;
+
+            if (result.tag.length) {
+                html += `<a href='/tag/${ encodeURIComponent(result.tag[0].toLocaleLowerCase()) }'>${result.tag[0]}</a>`;
+            };
+
+            html += `
+                        <time class="post-date">
+                                ${ result.date }
+                            </time>
+                        </section>
+                    </article>
+                `;
+            item.innerHTML = html;
+
+            searchResult.appendChild(item);
+        });
     }
-    return result
-  }
-
-  var fuseOptions = {
-    shouldSort: true,
-    includeMatches: true,
-    threshold: 0.0,
-    tokenize: true,
-    location: 0,
-    distance: 100,
-    maxPatternLength: 32,
-    minMatchCharLength: 1,
-    keys: [{
-        name: "title",
-        weight: 0.8
-      },
-      {
-        name: "summary",
-        weight: 0.5
-      },
-      {
-        name: "contents",
-        weight: 0.5
-      },
-      {
-        name: "tags",
-        weight: 0.3
-      },
-      {
-        name: "categories",
-        weight: 0.3
-      },
-      {
-        name: "date",
-        weight: 0.3
-      },
-    ]
-  };
-
-  let fuse = null
-
-  async function getFuse() {
-    if (fuse == null) {
-      const resp = await fetch('/index.json', {
-        method: 'get'
-      })
-      const indexData = await resp.json()
-      console.log(indexData.length)
-      fuse = new Fuse(indexData, fuseOptions);
-    }
-    return fuse
-  }
-
-  function render(items) {
-    return items.map(item => {
-      item = item.item
-      return `<li class="post-item">
-        <a href="${item.permalink}">
-          <span class="post-title">${item.title}</span>
-          <span
-            class="post-day">${item.date}</span>
-        </a>
-      </li>`}).join('')
-  }
-
-  function updateDOM(html, number) {
-    searchResult.innerHTML = html
-    searchCount.innerHTML = `共有查询到${number}篇文章`
-  }
-
-  async function search() {
-    const searchString = searchInput.value
-    const fuse = await getFuse()
-    const result = fuse.search(searchString)
-    const html = render(result)
-    updateDOM(html, result.length)
-  }
-
-  function doSearch() {
-    const wd = parseLocationSearch()['wd'] || ''
-    searchInput.value = wd
-    if (wd) {
-      search()
-    }
-  }
-
-  function goSearch() {
-    if (searchInput.value == parseLocationSearch()['wd']) {
-      return
-    }
-    history.pushState('', '', location.pathname + '?wd=' + searchInput.value)
-    searchInput.blur();
-    doSearch()
-  }
-
-  searchButton.addEventListener('click', goSearch)
-  searchInput.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-      goSearch()
-    }
-  })
-
-  doSearch()
-
-  window.addEventListener('popstate', doSearch)
-
-})
+}
