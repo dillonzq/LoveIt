2eda286bb56d7e4555432b6d21aa7eaf5d97ad9f
kbyyd24 <melo@gaoyuexiang.cn>
2022-02-27 20:48:05 +0800

    fix toc issue with adoc post

--- src/js/theme.js
+++ src/js/theme.js
@@ -387,7 +387,7 @@ class Theme {
 
     initHeaderLink() {
         for (let num = 1; num <= 6; num++) {
-            this.util.forEach(document.querySelectorAll('.single .content > h' + num), $header => {
+            this.util.forEach(document.querySelectorAll('.single .content h' + num), $header => {
                 $header.classList.add('headerLink');
                 $header.insertAdjacentHTML('afterbegin', `<a href="#${$header.id}" class="header-mark"></a>`);
             });
