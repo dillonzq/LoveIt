c51e292f9406cf55bc1e1953fbed4493860823e3
Dillon <dillonzq@outlook.com>
2025-02-23 09:41:38 +0800

    chore(build): update build system (#920)

--- assets/lib/lunr/lunr.segmentit.js
+++ assets/lib/lunr/lunr.segmentit.js
@@ -172,10 +172,8 @@ n.CHS_NAMES=T,n.ChsNameOptimizer=Z,n.ChsNameTokenizer=k,n.DatetimeOptimizer=$,n.
 "use strict";
 
 var _segmentit = require("segmentit");
-
 var segmentit = (0, _segmentit.useDefault)(new _segmentit.Segment());
 lunr.segmentit = segmentit;
-
 lunr.queryHandler = function (query) {
   if (/^[\u4e00-\u9fa5]+$/.test(query)) query = lunr.segmentit.doSegment(query).map(function (seg) {
     return '+' + seg.w;
